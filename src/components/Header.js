765eddf9d2d106aeedfa3277196a3d75d74bc713
Rayping <ccps99001@gmail.com>
2024-03-01 20:11:50 +0800

    hw

---
+++ src/components/Header.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const Header = () => {
+  return (
+      <View style={styles.headerStyle}>
+        <Text style={styles.textStyle}>合輯</Text>
+      </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  headerStyle: {
+    backgroundColor: "#2894FF",
+    justifyContent: "center",
+    alignItems: "center",
+    height: 60,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    // Android Only
+    elevation: 4
+  },
+  textStyle: {
+    fontSize: 24
+  },
+});
+
+export default Header;
